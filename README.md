52df16bbd317032a8d80df9a5f4d722730f40f81
PhoenixDev22 <phoenix.developper@gmail.com>
2022-06-10 18:10:38 +0100

    First Submit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Frontend Mentor - Fylo landing page with two column layout solution
+
+This is a solution to the [Fylo landing page with two column layout challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/fylo-landing-page-with-two-column-layout-5ca5ef041e82137ec91a50f5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+
+
+## Overview
+
+### The challenge
+
+
+
+### Screenshot
+
+![](images/fylo-web-capture.jpeg)
+
+### Links
+
+- Solution URL: [Solution URL here](https://github.com/PhoenixDev22/Frontend-Mentor-Fylo-landing-page-with-two-column-layout)
+- Live Site URL: [Live site URL here hosted on Netlify](https://fylo-landing-page-phoenixdev22.netlify.app/)
+
+## My process
+
+```
+const validRegExp = /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})
+
+*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i;
+
+```
+
+[This email Regexpression was copied from here.](https://herewecode.io/blog/email-validation-javascript/#:~:text=The%20most%20common%20way%20to%20validate%20an%20email,an%20email%20is%20a%20string%20following%20this%20format%3A)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+
+
+
+### Continued development
+
+- Focusing on accessiblity. 
+- Adding more animations to the page.
+- Adding minified files.
+- Minimizing the code.
+- Learning BEM convention.
+
+
+## Author
+
+- Frontend Mentor - [@PhoenixDev22](https://www.frontendmentor.io/profile/PhoenixDev22)
+- Github - [PhoenixDev22](https://github.com/PhoenixDev22?tab=repositories)
+
+
---
+++ index.js
@@ -0,0 +1,52 @@
+const scrollUpBtn = document.getElementById(`scroll-up`);
+const heroForm  = document.getElementById("hero__form")
+const signForm  = document.getElementById("sign__form")
+let isValid = false;
+
+const validRegExp = /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i;
+
+
+window.onscroll = () => window.scrollY >= 600? scrollUpBtn.classList.add(`visible-btn`): scrollUpBtn.classList.remove(`visible-btn`);
+
+scrollUpBtn.addEventListener(`click`, () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+
+heroForm.addEventListener(`submit`, event => checkEmailValidation(event))
+signForm.addEventListener(`submit`, event => checkEmailValidation(event))
+
+function checkEmailValidation (e){
+    e.preventDefault();
+    // Get the email  field of the current form
+    let currentEmail = e.currentTarget.children[0].children[1];
+    // Get the current value of the  email field of the current form 
+    let currentEmailValue = e.currentTarget.email.value;
+    // Check validation 
+    isValid = validRegExp.test(currentEmailValue)
+    let currentValidationMsg = e.currentTarget.children[0].children[2];
+
+    if(!isValid){
+        currentValidationMsg.removeAttribute("hidden")
+        currentValidationMsg.textContent = `Please check your email`;
+        currentValidationMsg.classList.remove(`valid__msg`);
+        currentValidationMsg.classList.add(`validation__msg`);
+        currentEmail.classList.add(`red-border`)
+    }else{
+        currentValidationMsg.removeAttribute("hidden")
+        currentValidationMsg.textContent = `Your email is successfully submited`;
+        currentValidationMsg.classList.remove(`validation__msg`);
+        currentValidationMsg.classList.add(`valid__msg`);
+        currentEmail.classList.add(`green-border`)
+    }
+    setTimeout(() => {
+        currentValidationMsg.setAttribute(`hidden`, `true`);
+        currentEmail.classList.remove(`red-border`);
+        currentEmail.classList.remove(`green-border`)
+    }, 2500)
+}
+
+
